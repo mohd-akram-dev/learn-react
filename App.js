8c6496ddd1ab089a701219c72a165cafe6629f8a
Mohd Akram <mohd.akram@routadigital.com>
2026-05-25 15:38:04 +0530

    episode one

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
-// const heading = React.createElement("h1", {
-//     id: "heading",
-//     className: "headings"
-// }, "Hello World from React!");
+/**
+ * <div id="parent">
+ *  <div id="child">
+ *  <h1 id="sibOne">I am first Sibling</h1>
+ *  <h2 id="sibTwo">I am Second Sibling</h2>
+ *  </div>
+ *  <div id="child2">
+ *   <h3 id="sibOne1">I am first Sibling</h3>
+ *   <h4 id="sibTwo2">I am Second Sibling</h4>
+ *  </div>
+ * </div>
+ */
 
 const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child" },
-        [
-            React.createElement("h1", {}, "Hi I am H1 from child 1 Tag"),
-            React.createElement("h2", {}, "Hi I am H2 from child 1 Tag")
-        ]
-    ),
-    React.createElement("div", { id: "child2" },
-        [
-            React.createElement("h1", {}, "Hi I am H1 from child 2 Tag"),
-            React.createElement("h2", {}, "Hi I am H2 from child 2 Tag")
-        ]
-    )
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "sibOne" }, "I am first Sibling"),
+    React.createElement("h2", { id: "sibTwo" }, "I am Second Sibling"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h3", { id: "sibOne1" }, "I am first Sibling"),
+    React.createElement("h4", { id: "sibTwo2" }, "I am Second Sibling"),
+  ]),
 ]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(parent);
